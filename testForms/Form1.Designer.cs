7a35256158172523a2c0656d8b5236c48d1f9357
herruber <robertnyquist@hotmail.com>
2018-05-22 17:22:52 +0200

    Pusta paylo

--- testForms/Form1.Designer.cs
+++ testForms/Form1.Designer.cs
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Scan = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RESET = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 381);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(441, 29);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(149, 484);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 444);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(423, 98);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Scan
+            // 
+            this.Scan.Location = new System.Drawing.Point(605, 29);
+            this.Scan.Name = "Scan";
+            this.Scan.Size = new System.Drawing.Size(86, 23);
+            this.Scan.TabIndex = 3;
+            this.Scan.Text = "Start Scan";
+            this.Scan.UseVisualStyleBackColor = true;
+            this.Scan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(423, 20);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(605, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save Changes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // RESET
+            // 
+            this.RESET.BackColor = System.Drawing.Color.Red;
+            this.RESET.Location = new System.Drawing.Point(607, 459);
+            this.RESET.Name = "RESET";
+            this.RESET.Size = new System.Drawing.Size(86, 54);
+            this.RESET.TabIndex = 6;
+            this.RESET.Text = "RESET";
+            this.RESET.UseVisualStyleBackColor = false;
+            this.RESET.Click += new System.EventHandler(this.RESET_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(605, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Load Image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(709, 29);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(423, 381);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(605, 351);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 59);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Generate";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1226, 554);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.RESET);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Scan);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Scan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button RESET;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- testForms/Form1.cs
+++ testForms/Form1.cs
@@ -1,20 +1,450 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace testForms
 {
     public partial class Form1 : Form
     {
+
+        public class RGB
+        {
+            public int R = 0;
+            public int G = 0;
+            public int B = 0;
+
+            public RGB()
+            {
+
+            }
+
+            public RGB(int r, int g, int b)
+            {
+                R = r;
+                G = g;
+                B = b;
+            }
+
+            public RGB(Color color)
+            {
+                R = color.R;
+                G = color.G;
+                B = color.B;
+            }
+
+            public RGB(CustomColor color)
+            {
+                R = (int)color.R;
+                G = (int)color.G;
+                B = (int)color.B;
+
+            }
+        }
+
+        public class ColorChart
+        {
+            public string FamilyName { get; set; }
+            public RGB Min { get; set; }
+            public RGB Max { get; set; }
+            public List<CustomColor> Colors = new List<CustomColor>();
+
+            public ColorChart()
+            {
+                Min = new RGB(9999, 9999, 9999);
+                Max = new RGB();
+            }
+
+            public void AddColor(CustomColor color)
+            {
+                Colors.Add(color);
+
+                Min.R = Math.Min((int)color.R, Min.R);
+                Min.G = Math.Min((int)color.G, Min.G);
+                Min.B = Math.Min((int)color.B, Min.B);
+
+                Max.R = Math.Max((int)color.R, Min.R);
+                Max.G = Math.Max((int)color.G, Min.G);
+                Max.B = Math.Max((int)color.B, Min.B);
+            }
+
+        }
+
+        public class CustomColor
+        {
+            public string Name { get; set; }
+            public string Family { get; set; }
+            public Color _Color { get; set; }
+
+            public double R { get; set; }
+            public double G { get; set; }
+            public double B { get; set; }
+
+            public double Hue { get; set; }
+            public double Saturation { get; set; }
+            public double Brightness { get; set; }
+
+            public CustomColor(double r, double g, double b, string name)
+            {
+                Name = name;
+                R = r;
+                G = g;
+                B = b;
+
+                _Color = Color.FromArgb((int)r, (int)g, (int)b);
+
+                Hue = _Color.GetHue();
+                Brightness = _Color.GetBrightness();
+                Saturation = _Color.GetSaturation();
+
+            }
+
+            public CustomColor(Color color)
+            {
+                R = (int)color.R;
+                G = (int)color.G;
+                B = (int)color.B;
+
+                _Color = Color.FromArgb((int)R, (int)G, (int)B);
+
+                Hue = _Color.GetHue();
+                Brightness = _Color.GetBrightness();
+                Saturation = _Color.GetSaturation();
+            }
+
+            public CustomColor()
+            {
+                Name = "Black";
+                R = 0;
+                G = 0;
+                B = 0;
+
+                _Color = Color.Black;
+
+                Hue = _Color.GetHue();
+                Brightness = _Color.GetBrightness();
+                Saturation = _Color.GetSaturation();
+            }
+        }
+
+        public static List<string> ColorFamilies = new List<string> {
+            "Brown",
+            "Black",
+            "White",
+            "Yellow",
+            "Orange",
+            "Gray",
+            "Pink",
+            "Purple",
+            "Cyan",
+            "Red",
+            "Green",
+            "Blue"
+        };
+
+        public CustomColor MatchColors(Color pixel)
+        {
+
+            return new CustomColor();
+        }
+
+
+        private CustomColor MatchColors(CustomColor pixel)
+        {
+
+
+            for (int ca = 0; ca < Charts.Count; ca++)
+            {
+                var chart = Charts[ca];
+
+                for (int col = 0; col < chart.Colors.Count; col++)
+                {
+                    var color = chart.Colors[col];
+
+                    double diffHue = Math.Min(Math.Abs(pixel.Hue - color.Hue), 360 - Math.Abs(pixel.Hue - color.Hue));
+                    double diffSat = Math.Abs(pixel.Saturation - color.Saturation);
+                    double diffBright = Math.Abs(pixel.Brightness - color.Brightness);
+
+                    if (pixel.Saturation < 0.1) //Concider grayscale
+                    {
+                        if (diffSat < 0.1 && diffBright < 0.1)
+                        {
+                            return color;
+                        }
+                    }
+                    else
+                    {
+                        if (diffHue < 15)
+                        {
+                            return color;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        List<CustomColor> undefinedColors = new List<CustomColor>();
+        CustomColor undefinedColor;
+        int lastY = -1;
+        int lastX = -1;
+
+        public void ScanPixels()
+        {
+
+            Bitmap bmap = new Bitmap(pictureBox1.ImageLocation);
+            undefinedColor = null;
+
+            if (lastY >= bmap.Height - 2)
+            {
+                pictureBox2.BackColor = Color.White;
+                return;
+            }
+
+            for (int y = lastY + 1; y < bmap.Height; y++)
+            {
+
+                for (int x = lastX + 1; x < bmap.Width; x++)
+                {
+                    Color color = bmap.GetPixel(x, y);
+
+                    //If color does not exist, promt to create it
+                    if (MatchColors(new CustomColor(color)) == null) {
+
+                        //undefinedColors.Add(new CustomColor(color));
+                        undefinedColor = new CustomColor(color);
+                        lastX = x;
+                        lastY = y;
+                        return;
+                    };
+                }
+            }
+        }
+
+        public static List<ColorChart> Charts = new List<ColorChart>();
+
+        public void CreateUpdateFiles()
+        {
+
+            string[] files = Directory.GetFiles("../../ColorDefinitions");
+            
+
+            for (int cf = 0; cf < ColorFamilies.Count; cf++)
+            {
+                string familyName = ColorFamilies[cf];
+                bool create = true;
+
+                for (int f = 0; f < files.Length; f++)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(files[f]);
+
+                    if (fileName.ToLower() == familyName.ToLower())
+                    {
+                        create = false;
+                        break;
+                    }
+                }
+
+                if (create)
+                {
+
+
+                    ColorChart chart = new ColorChart();
+                    chart.FamilyName = familyName;
+
+                    using (StreamWriter file = File.CreateText("../../ColorDefinitions/" + familyName + ".json"))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        serializer.Formatting = Formatting.Indented;
+                        serializer.Serialize(file, chart);
+                    }
+                }
+            }
+        }
+
+        public void SaveCharts()
+        {
+            for (int c = 0; c < Charts.Count; c++)
+            {
+
+                var chart = Charts[c];
+                string familyName = chart.FamilyName;
+
+                using (StreamWriter file = File.CreateText("../../ColorDefinitions/" + familyName + ".json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Formatting = Formatting.Indented;
+                    serializer.Serialize(file, chart);
+                }
+            }
+        }
+
+        public void ResetCharts()
+        {
+            for (int c = 0; c < ColorFamilies.Count; c++)
+            {
+                var familyName = ColorFamilies[c];
+
+                var chart = new ColorChart();
+                chart.FamilyName = familyName;
+                Charts[c] = chart;
+            }
+
+            
+        }
+
+        public void LoadCharts()
+        {
+            checkedListBox1.Items.Clear();
+
+            for (int i = 0; i < ColorFamilies.Count; i++)
+            {
+                string familyName = ColorFamilies[i];
+                checkedListBox1.Items.Add(familyName, 0);
+                using (StreamReader file = File.OpenText("../../ColorDefinitions/" + familyName + ".json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    ColorChart chart = (ColorChart)serializer.Deserialize(file, typeof(ColorChart));
+
+                    Charts.Add(chart);
+                }
+            }
+        }
+
+        public void UpdateSelector()
+        {
+            if (undefinedColor != null)
+            {
+                pictureBox2.BackColor = undefinedColor._Color;
+            }
+            
+            
+        }
+
         public Form1()
         {
             InitializeComponent();
+            this.pictureBox1.ImageLocation = "../../7190d69bd90870ec2551302fbe4e205b.jpg";
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //Create or update missing colorcharts, incase they have been changed
+            CreateUpdateFiles();
+
+            //Load charts
+            LoadCharts();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ScanPixels();
+
+            UpdateSelector();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (undefinedColor != null)
+            {
+                for (int ca = 0; ca < Charts.Count; ca++)
+                {
+                    if (Charts[ca].FamilyName == (string)checkedListBox1.SelectedItem)
+                    {
+                        undefinedColor.Family = Charts[ca].FamilyName;
+                        Charts[ca].AddColor(undefinedColor);
+                        ScanPixels();
+                        UpdateSelector();
+                        checkedListBox1.ClearSelected();
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                pictureBox1.Image = null;
+
+            }
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SaveCharts();
+        }
+
+        private void RESET_Click(object sender, EventArgs e)
+        {
+            ResetCharts();
+            SaveCharts();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = openFileDialog1)
+            {
+                dlg.InitialDirectory = Application.StartupPath;
+                dlg.Title = "Open Image";
+                dlg.Filter = "All files(*.*) | *.*";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    PictureBox PictureBox1 = new PictureBox();
+
+                    // Create a new Bitmap object from the picture file on disk,
+                    // and assign that to the PictureBox.Image property
+                    pictureBox1.Image = new Bitmap(dlg.FileName);
+                    pictureBox1.ImageLocation = dlg.FileName;
+                    lastY = -1;
+                    lastX = -1;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox1.Image != null && pictureBox1.ImageLocation.Length > 0)
+            {
+
+                Bitmap bmp = new Bitmap(pictureBox1.ImageLocation);
+                Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+
+
+                for (int y = 0; y < bmp2.Height; y++)
+                {
+
+                    for (int x = 0; x < bmp2.Width; x++)
+                    {
+
+                        var pixel = bmp.GetPixel(x, y);
+                        CustomColor color = MatchColors(new CustomColor(pixel));
+
+                        if (color != null)
+                        {
+                            bmp2.SetPixel(x, y, color._Color);
+                        }
+                    }
+                }
+
+                pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox3.Image = bmp2;
+
+            }
         }
     }
 }
